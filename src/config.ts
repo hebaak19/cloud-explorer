d99734d0e2dac211df23886958435ad13a3ca332
hebaak19 <hebaalkathiri9@gmail.com>
2026-08-03 14:31:19 +0300

    test

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export type CameraMode = "vintage" | "modern" | "polaroid";
 export const CONFIG = {
   gradientColors: ["#ff0000", "#ff9900", "#ffff00"],
 
-  name: "Heba Adel",
+  name: "Heba Adel ai",
 
   funFact: "I hate to pay my bills.",
 
